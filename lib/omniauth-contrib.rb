94f46302000363d60c204a829801b39252fd6a4c
Adam Weller <mincheastwood@gmail.com>
2011-12-07 10:04:52 -0500

    first attempt at google apps strategy for latest omniauth

--- lib/omniauth-contrib.rb
+++ lib/omniauth-contrib.rb
@@ -4,6 +4,7 @@ require "omniauth"
 module OmniAuth
   module Strategies
     autoload :VKontakte,  'omniauth/strategies/vkontakte'
+    autoload :GoogleApps,  'omniauth/strategies/google_apps'
   end
 end
 
---
+++ lib/omniauth/strategies/google_apps.rb
@@ -0,0 +1,65 @@
+require 'omniauth-openid'
+require 'gapps_openid'
+
+module OpenID
+  # Because gapps_openid changes the discovery order
+  # (looking first for Google Apps, then anything else),
+  # we need to monkeypatch it to make it play nicely
+  # with others.
+  def self.discover(uri)
+    discovered = self.default_discover(uri)
+
+    if discovered.last.empty?
+      info = discover_google_apps(uri)
+      return info if info
+    end
+
+    return discovered
+  rescue OpenID::DiscoveryFailure => e
+    Rails.logger.error "OpenId::DiscoveryFailure:  #{e.inspect}"
+    info = discover_google_apps(uri)
+
+    if info.nil?
+      raise e
+    else
+      return info
+    end
+  end
+
+  def self.discover_google_apps(uri)
+    discovery = GoogleDiscovery.new
+    discovery.perform_discovery(uri)
+  end
+end
+
+module OmniAuth
+  module Strategies
+    class GoogleApps < OmniAuth::Strategies::OpenID
+      def initialize(app, store = nil, options = {}, &block)
+        options[:name] ||= 'google_apps'
+        
+        hash = {
+          app: app,
+          store: store,
+          options: options
+        }
+        Rails.logger.debug ">>>>>>>>> hash = #{hash.inspect}"
+        
+        super(app, options, &block)
+      end
+
+      def get_identifier
+        OmniAuth::Form.build(:title => 'Google Apps Authentication') do
+          label_field('Google Apps Domain', 'domain')
+          input_field('url', 'domain')
+        end.to_response
+      end
+
+      def identifier
+        id = options[:domain] || request['domain']
+        Rails.logger.debug ">>>>>>>> id = #{id.inspect}"
+        id
+      end
+    end
+  end
+end
